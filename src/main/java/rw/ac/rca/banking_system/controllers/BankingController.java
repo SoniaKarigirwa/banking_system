caa3eaaa28448a728eeb145571551dd91016adf7
SoniaKarigirwa <karigirwasonia0@gmail.com>
2024-06-25 15:56:07 +0400

    Added banking contrller

--- src/main/java/rw/ac/rca/banking_system/controllers/BankingController.java
+++ src/main/java/rw/ac/rca/banking_system/controllers/BankingController.java
@@ -1,4 +1,125 @@
 package rw.ac.rca.banking_system.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import jakarta.validation.Valid;
+import rw.ac.rca.banking_system.dtos.SavingDTO;
+import rw.ac.rca.banking_system.dtos.TransferDTO;
+import rw.ac.rca.banking_system.dtos.WithdrawDTO;
+import rw.ac.rca.banking_system.enums.BankingType;
+import rw.ac.rca.banking_system.models.Customer;
+import rw.ac.rca.banking_system.services.BankingService;
+import rw.ac.rca.banking_system.services.CustomerService;
+import rw.ac.rca.banking_system.validaton.InputValidator;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static rw.ac.rca.banking_system.enums.ResponseType.*;
+
+
+@RestController
+@RequestMapping("api/v1")
 public class BankingController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(BankingController.class);
+
+    private final BankingService bankingService;
+    private final CustomerService customerService;
+
+    @Autowired
+    public BankingController(CustomerService customerService, BankingService bankingService) {
+        this.customerService = customerService;
+        this.bankingService = bankingService;
+    }
+
+    @PostMapping(value = "/transactions",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> makeTransfer(
+            @Valid @RequestBody TransferDTO transferDTO) {
+        if (InputValidator.isSearchTransactionValid(transferDTO)) {
+//            new Thread(() -> transactionService.makeTransfer(transactionInput));
+            boolean isComplete = bankingService.makeTransfer(transferDTO);
+            return new ResponseEntity<>(isComplete, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(INVALID_TRANSACTION, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping(value = "/withdraw",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> withdraw(
+            @Valid @RequestBody WithdrawDTO withdrawDTO) {
+        LOGGER.debug("Triggered AccountRestController.withdrawInput");
+
+        // Validate input
+        if (InputValidator.isSearchCriteriaValid(withdrawDTO)) {
+            // Attempt to retrieve the account information
+            Customer customer = customerService.getCustomer(
+                    withdrawDTO.getAccount());
+
+            // Return the account details, or warn that no account was found for given input
+            if (customer == null) {
+                return new ResponseEntity<>(NO_CUSTOMER_FOUND, HttpStatus.OK);
+            } else {
+                if (bankingService.isAmountAvailable(withdrawDTO.getAmount(), customer.getBalance())) {
+                    bankingService.updateAccountBalance(customer, withdrawDTO.getAmount(), BankingType.WITHDRAW);
+                    return new ResponseEntity<>(SUCCESS, HttpStatus.OK);
+                }
+                return new ResponseEntity<>(INSUFFICIENT_ACCOUNT_BALANCE, HttpStatus.OK);
+            }
+        } else {
+            return new ResponseEntity<>(INVALID_SEARCH_CRITERIA, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+
+    @PostMapping(value = "/deposit",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> deposit(
+            @Valid @RequestBody SavingDTO savingDTO) {
+        LOGGER.debug("Triggered AccountRestController.depositInput");
+
+        // Validate input
+        if (InputValidator.isAccountNoValid(savingDTO.getTargetCustomer())) {
+            // Attempt to retrieve the account information
+            Customer customer = customerService.getCustomer(savingDTO.getTargetCustomer());
+
+            // Return the account details, or warn that no account was found for given input
+            if (customer == null) {
+                return new ResponseEntity<>(NO_CUSTOMER_FOUND, HttpStatus.OK);
+            } else {
+                bankingService.updateAccountBalance(customer, savingDTO.getAmount(), BankingType.SAVING);
+                return new ResponseEntity<>(SUCCESS, HttpStatus.OK);
+            }
+        } else {
+            return new ResponseEntity<>(INVALID_SEARCH_CRITERIA, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        return errors;
+    }
 }
--- src/main/java/rw/ac/rca/banking_system/dtos/WithdrawDTO.java
+++ src/main/java/rw/ac/rca/banking_system/dtos/WithdrawDTO.java
@@ -1,4 +1,48 @@
 package rw.ac.rca.banking_system.dtos;
 
-public class WithdrawDTO {
+import jakarta.validation.constraints.Positive;
+
+import java.util.Objects;
+
+public class WithdrawDTO extends CustomerDTO{
+
+    String account;
+
+    // Prevent fraudulent transfers attempting to abuse currency conversion errors
+    @Positive(message = "Transfer amount must be positive")
+    private double amount;
+
+    public WithdrawDTO() {
+        this.account = super.getAccount();
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return "CustomerDTO{" +
+                ", accountNumber='" + account + '\'' +
+                ", amount='" + amount + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        WithdrawDTO that = (WithdrawDTO) o;
+        return Objects.equals(account, that.account) &&
+                Objects.equals(amount, that.amount);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(account, amount);
+    }
 }
